a7e193c81b07e63b0c82e11b3cfb38db45cf4fd6
kaemil <67312346+kaemil@users.noreply.github.com>
2021-10-09 11:54:44 +0200

    Small code correction

--- src/App.js
+++ src/App.js
@@ -1,60 +1,60 @@
-import React,{useState,useRef, useEffect} from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import ShopList from './components/shopList';
 import ShopPanel from './components/shopPanel';
-import './style/style.css'
+import './style/style.css';
 
 function App() {
-    const [quantity,setQuantity] = useState(1)
-    const [productQuantity,setProductQuantity] = useState([])
+	const [quantity, setQuantity] = useState(1);
+	const [productQuantity, setProductQuantity] = useState([]);
 
-    const [product,setProduct] = useState('')
-    const [productList,setProductList] = useState([])
+	const [product, setProduct] = useState('');
+	const [productList, setProductList] = useState([]);
 
-    const [choosenUnit,setChoosenUnit] = useState('-')
-    const [unit,setUnit] = useState([])
-    const inputFocus = useRef()
+	const [choosenUnit, setChoosenUnit] = useState('-');
+	const [unit, setUnit] = useState([]);
+	const inputFocus = useRef();
 
-    useEffect(()=>inputFocus.current.focus(),[])
+	useEffect(() => inputFocus.current.focus(), []);
 
-    const handleChange = (e) => {
-        if(e.target.name === 'product') {
-            setProduct(e.target.value)
-        } else if(e.target.name === 'quantity'){
-            setQuantity(e.target.value)
-        } else 
-            setChoosenUnit(e.target.value)}
-        
-    const handleClick = (e,id) =>{
-        if(e.target.name === 'add'){
-            setUnit([...unit,choosenUnit])
-            setProductList([...productList,product])
-            setProduct('')
-            setQuantity(1)
-            setProductQuantity([...productQuantity, quantity])
-            inputFocus.current.focus()
-        } else {
-            productList.splice(id,1)
-            setProductList([...productList])
-        }
-    }
+	const handleChange = (e) => {
+		if (e.target.name === 'product') {
+			setProduct(e.target.value);
+		} else if (e.target.name === 'quantity') {
+			setQuantity(e.target.value);
+		} else setChoosenUnit(e.target.value);
+	};
 
-    return (
-    <div>
-        <ShopPanel
-            inputFocus={inputFocus}
-            handleChange={handleChange}
-            product={product}
-            handleClick={handleClick}
-            quantity={quantity}
-        />
-        <ShopList
-            unit={unit}
-            productQuantity={productQuantity}
-            handleClick={handleClick}
-            productList={productList}
-        />
-    </div>
-    );
+	const handleClick = (e, id) => {
+		if (e.target.name === 'add') {
+			setUnit([...unit, choosenUnit]);
+			setProductList([...productList, product]);
+			setProduct('');
+			setQuantity(1);
+			setProductQuantity([...productQuantity, quantity]);
+			inputFocus.current.focus();
+		} else {
+			productList.splice(id, 1);
+			setProductList([...productList]);
+		}
+	};
+
+	return (
+		<div>
+			<ShopPanel
+				inputFocus={inputFocus}
+				handleChange={handleChange}
+				product={product}
+				handleClick={handleClick}
+				quantity={quantity}
+			/>
+			<ShopList
+				unit={unit}
+				productQuantity={productQuantity}
+				handleClick={handleClick}
+				productList={productList}
+			/>
+		</div>
+	);
 }
 
 export default App;
--- src/components/shopList.js
+++ src/components/shopList.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import ShopListItem from './shopListItem';
 
-function ShopList({productList,productQuantity,unit,handleClick}){
-        // Rozbić na componenty
-    const products = productList.map((product,index)=>{
-        return <ShopListItem 
-                    quantity={productQuantity[index]}
-                    unit={unit[index]}
-                    key={index}
-                    id={index}
-                    product={product} 
-                    handleClick={handleClick}
-                />
-    })
-    return (
-        <div>
-            <table>
-                <tbody>
-                {products}
-                </tbody>
-            </table>
-        </div>
-      );
+function ShopList({ productList, productQuantity, unit, handleClick }) {
+	// Rozbić na componenty
+	const products = productList.map((product, index) => {
+		return (
+			<ShopListItem
+				quantity={productQuantity[index]}
+				unit={unit[index]}
+				key={index}
+				id={index}
+				product={product}
+				handleClick={handleClick}
+			/>
+		);
+	});
+	return (
+		<div>
+			<table>
+				<tbody>{products}</tbody>
+			</table>
+		</div>
+	);
 }
 
-export default ShopList
+export default ShopList;
